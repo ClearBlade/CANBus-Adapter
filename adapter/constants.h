687a06dbe727123526f9ccf47c0e4681e32a219d
rreinold <rreinold@gmail.com>
2017-03-28 11:15:38 -0500

    Updated default values, and README.md

--- adapter/constants.h
+++ adapter/constants.h
@@ -2,13 +2,12 @@
 #define _CONSTANTS_H
 
 
-char *SYSTEM_KEY = "94bec4890bc0afe1fbd282b1ac8e01";
-char *SYSTEM_SECRET = "94BEC4890BC68AB6B9BBEDF6B21F";
-char *USER_EMAIL = "test@test.com";
-char *USER_PASSWORD = "test";
-char *PLATFORM_URL = "https://platform.clearblade.com";
+char *SYSTEM_KEY = <SYSTEM_KEY>; // ex. "94aaa4890bc0afe1fbd282b1ac8e01"
+char *SYSTEM_SECRET = <SYSTEM_SECRET>; // ex. "9AAEC4890BC68AB6B9BBEDF6B21F"
+char *USER_EMAIL = <USER_EMAIL>; // ex. "test@clearblade.com"
+char *USER_PASSWORD = <PASSWORD>; // ex. "password"
+char *PLATFORM_URL = "https://platform.clearblade.com"; // ex.
 char *MESSAGING_URL = "tcp://platform.clearblade.com:1883";
 char *MQTT_CLIENT_ID = "canbus-adapter";
-
 
 #endif
